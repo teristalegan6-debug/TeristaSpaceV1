8a0eee87eee8f6944564607fd3f28f393017207a
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 22:55:48 +0530

    Update Application.mk

--- native/src/main/cpp/Application.mk
+++ native/src/main/cpp/Application.mk
@@ -1,1 +1,6 @@
+APP_ABI := arm64-v8a armeabi-v7a
+APP_PLATFORM := android-24
+APP_STL := c++_static
+APP_CPPFLAGS := -std=c++17 -fexceptions -frtti
+APP_CFLAGS := -DANDROID -ffunction-sections -fdata-sections
 
